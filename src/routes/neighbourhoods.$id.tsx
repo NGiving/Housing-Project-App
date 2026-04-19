a8082f3b9a4429e16d8e798359e6e905e51f350d
luca <pereluca10@gmail.com>
2026-04-19 17:33:50 -0400

    feat: new way to display neighbourhood snapshot

--- src/routes/neighbourhoods.$id.tsx
+++ src/routes/neighbourhoods.$id.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+const snapshots = import.meta.glob('../assets/snapshots/*.svg', {
+  eager: true,
+  query: '?url',
+  import: 'default',
+}) as Record<string, string>
+
 export const Route = createFileRoute('/neighbourhoods/$id')({
-  component: RouteComponent,
+  component: Neighbourhoods_ID,
 })
 
-function RouteComponent() {
-  return <div>Hello "/neighbourhoods/$id"!</div>
+function Neighbourhoods_ID() {
+  const { id } = Route.useParams()
+  const snapshotSrc = snapshots[`../assets/snapshots/${id}.svg`]
+
+  return !snapshotSrc ?
+    <div className="container max-w-5xl mx-auto px-8 py-6">
+      <h1 className="text-center">Neighbourhood not found</h1>
+    </div>
+    :
+    <div className="max-w-5xl mx-auto px-8 py-6">
+      <img src={snapshotSrc} alt={id} className="w-full h-auto" />
+    </div>
 }
